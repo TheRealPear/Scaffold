9816889a0f2fe15a4257c28f5f8ec8d861862bf6
Mew2K <MewTwoKing5@gmail.com>
2022-06-12 11:58:01 -0500

    pom.xml update

---
+++ src/main/java/network/warzone/scaffold/commands/XmlCommands.java
@@ -0,0 +1,12 @@
+package network.warzone.scaffold.commands;
+
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandNumberFormatException;
+import com.sk89q.minecraft.util.commands.CommandPermissions;
+
+public class XmlCommands {
+
+
+
+}
